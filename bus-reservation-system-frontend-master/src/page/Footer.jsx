de9f19806cd412038e43ae46b1859caa79551a7c
Tadimarri Nasreen <tadimarrinasreen123@gmail.com>
2025-09-03 20:43:11 +0530

    Initial Capstone Project

---
+++ bus-reservation-system-frontend-master/src/page/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-light text-center py-3 mt-auto">
+      <div className="container">
+        <span className="text-muted">© Online Bus Booking System</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
